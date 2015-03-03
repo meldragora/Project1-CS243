e72a4562acdee08767bc4c537e703f1ec4fe7e3a
meldragora <meldragora@yahoo.com>
2015-03-03 17:07:51 -0500

    Method headers added

--- node_heap.c
+++ node_heap.c
@@ -1,1 +1,40 @@
+#include node_heap.h
+#include <stdio.h>
+#include <stdlib.h>
 
+#static compareNode
+
+int compareNode(Node parent, Node child)
+{
+  
+}
+
+void sift_up( Heap * heap, Node node)
+{
+  
+}
+
+int read_symbols( size_t maxcount, Symbol syms[] )
+{
+  
+}
+
+void heap_init( Heap * heap )
+{
+  
+}
+
+void heap_make( Heap * heap, size_t length, Symbol symlist[] )
+{
+  
+}
+
+void heap_add( Heap * heap, Node node )
+{
+  
+}
+
+Node heap_remove( Heap * heap )
+{
+  
+}
