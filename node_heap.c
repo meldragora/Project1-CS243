6d328db7714c3e41f1e5228062ee023ff95664ae
meldragora <meldragora@yahoo.com>
2015-03-03 19:49:34 -0500

    Update node_heap.c
    Finished heap_add, heap_remove, sift_up, sift_down

--- node_heap.c
+++ node_heap.c
@@ -4,19 +4,48 @@
 
 #static compareNode
 
-int compareNode(Node parent, Node child)
+int compareNode(Node n1, Node n2)
 {
-  
+  return(n1.frequency - n2.frequency);
 }
 
-void sift_up( Heap * heap, Node node)
+void sift_up( Heap * heap)
 {
-  
+  int p = heap.size-1;
+  while(p)
+  {
+    int parent = (p-1)/2;
+    if(compareNode(heap.array[p],heap.array[parent])){
+      break;
+    }
+    else
+    {
+      node temp = heap.array[p];
+      heap.array[p] = heap.array[parent];
+      heap.array[parent] = temp;
+      
+      p = parent
+    }
+  }
 }
 
-void sift_down( Heap * heap, Node node)
+int sift_down( Heap * heap)
 {
-  
+  int p = 0;
+  while( (2*p) + 1 < size)
+  {
+    int lc = (2*p)+1;
+    int rc = lc+1;
+    int minc = lc;
+    if(rc < size)
+      if(compareNode(heap.array[lc], heap.array[rc]))
+        minc = rc;
+    if(compareNode(heap.array[minc],heap.array[p])){
+      node temp = heap.array[p];
+      heap.array[p] = heap.array[minc];
+      heap.array[minc] = temp;
+    }
+  }
 }
 
 int read_symbols( size_t maxcount, Symbol syms[] )
@@ -36,10 +65,20 @@ void heap_make( Heap * heap, size_t length, Symbol symlist[] )
 
 void heap_add( Heap * heap, Node node )
 {
-  
+  if(heap.size < heap.capacity)
+  {
+    ++heap.size;
+    heap.array[size-1] = node;
+    sift_up(heap);
+  }
 }
 
 Node heap_remove( Heap * heap )
 {
-  
+  if(heap.size)
+  {
+    heap.array[0]=heap.array[size-1];
+    --heap.size;
+    sift_down(heap);
+  }
 }
